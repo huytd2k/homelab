2aecfc0a2923561051c0dde1dddc84a93dd05254
Huy Tran.D <43570745+huytd2k@users.noreply.github.com>
2024-11-24 15:14:57 +0000

    udpate

--- cdk8s/main.ts
+++ cdk8s/main.ts
@@ -3,19 +3,43 @@ import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
 
 export class CodeServer extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
-    new kplus.Deployment(this, 'code-server', {
+    // Deployment for code-server
+    const deployment = new kplus.Deployment(this, 'code-server', {
+      replicas: 1,
       containers: [
         {
           image: 'lscr.io/linuxserver/code-server:latest',
           ports: [
             {
-              number: 8080, // Container port
-              hostPort: 8080, // Map to the host port
+              number: 8080, // Expose container port 8080
             },
           ],
+        },
+      ],
+    });
+
+    // Service to expose code-server internally
+    const service = new kplus.Service(this, 'code-server-service', {
+      type: kplus.ServiceType.CLUSTER_IP, // Internal service
+      ports: [
+        {
+          port: 80,        // Service port
+          targetPort: 8080, // Target container port
+        },
+      ],
+      selector: deployment,
+    });
+
+    // Ingress to expose code-server externally
+    new kplus.Ingress(this, 'code-server-ingress', {
+      rules: [
+        {
+          host: 'code-server.local', // Set desired host
+          path: '/',
+          backend: kplus.IngressBackend.fromService(service),
         },
       ],
     });
@@ -23,5 +47,5 @@ export class CodeServer extends Chart {
 }
 
 const app = new App();
-new CodeServer(app, 'deployment');
+new CodeServer(app, 'my-home-lab');
 app.synth();
